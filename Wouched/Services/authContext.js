04a54a672e83f07ba3eaed5041b9479b9be964d8
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2023-12-13 17:18:17 +0100

    Refactor authContext.js file

--- Wouched/Services/authContext.js
+++ Wouched/Services/authContext.js
@@ -1,43 +1,46 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import{ jwtDecode } from 'jwt-decode';
+    import React, { createContext, useContext, useState, useEffect } from 'react';
+    import{ jwtDecode } from 'jwt-decode';
 
-const AuthContext = createContext(null);
+    const AuthContext = createContext(null);
 
-export const useAuth = () => useContext(AuthContext);
+    export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [authLoading, setAuthLoading] = useState(true);
-    const [token, setToken] = useState();
+    export const AuthProvider = ({ children }) => {
+        const [user, setUser] = useState(null);
+        const [authLoading, setAuthLoading] = useState(true);
+        const [token, setToken] = useState();
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decoded = jwtDecode(token);
-            if (decoded) {
-                setUser(decoded.user);
-                setToken(token)
-                setAuthLoading(false);
+        useEffect(() => {
+            const token = localStorage.getItem('token');
+            if (token) {
+                try {
+                    const decoded = jwtDecode(token);
+                    if (decoded) {
+                        setUser(decoded.user);
+                        setToken(token)
+                        setAuthLoading(false);
+                    }
+                } catch (error) {
+                    console.error("Erreur de décodage du JWT", error);
+                }
             }
-        }
-    }, []);
+        }, [token]);
+        const login = (token) => {
+            localStorage.setItem('token', token);
+            const decoded = jwtDecode(token);
+            setUser(decoded);
+        };
 
-    const login = (token) => {
-        localStorage.setItem('token', token);
-        const decoded = jwtDecode(token);
-        setUser(decoded);
+        const logout = () => {
+            localStorage.removeItem('token');
+            setUser(null);
+        };
+
+        
+
+        return (
+            <AuthContext.Provider value={{ user, login, logout,authLoading, token }}>
+                {children}
+            </AuthContext.Provider>
+        );
     };
-
-    const logout = () => {
-        localStorage.removeItem('token');
-        setUser(null);
-    };
-
-    
-
-    return (
-        <AuthContext.Provider value={{ user, login, logout,authLoading, token }}>
-            {children}
-        </AuthContext.Provider>
-    );
-};
